3a909d22acacc8d1c00ee54d976a8ca5fb00ac29
CodeMyGame1 <40456953+CodeMyGame1@users.noreply.github.com>
2024-07-22 22:49:36 +0530

    init arm.hpp

---
+++ include/arm.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "api.h"
+
+class Arm {
+    public:
+        pros::Motor arm_motor(10);
+
+        pros::motor_brake_mode_e brake_mode;
+
+        Arm(
+            std::int8_t arm_motor_port
+
+            , pros::motor_brake_mode_e arm_brake_mode
+        );
+
+        void arm_up();
+
+        void arm_down();
+}
