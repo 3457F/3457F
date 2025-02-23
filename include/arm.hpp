6bfcc9074a4feab5ee7cc4aa5073231a3bbf2ad7
AaravDoesRobotics <164962937+AaravDoesRobotics@users.noreply.github.com>
2025-02-23 15:50:21 -0800

    fix arm constants

--- include/arm.hpp
+++ include/arm.hpp
@@ -13,9 +13,9 @@ class Arm {
     public:
         static constexpr double INIT_POS = 0.0 * 100;
         static constexpr double START_POS = 18.0 * 100;
-        static constexpr double LOADIN_POS = 45.69420 * 100; // 45 actual; 45 -> 55 -> 50
+        static constexpr double LOADIN_POS = 25 * 100; // 45 actual; 45 -> 55 -> 50
         static constexpr double DUNK_POS = 100.0 * 100; 
-        static constexpr double SCORE_POS = 168.0 * 100;
+        static constexpr double SCORE_POS = 140.0 * 100;
         static constexpr double AUTON = 200.0 * 100;
         static constexpr double ALLIANCE_SCORE = 191.0 * 100;
         static constexpr double PREALLIANCE_SCORE = 170.0 * 100;
