03253055553bda884a7466bcacbe5a623007d1a8
AaravDoesRobotics <164962937+AaravDoesRobotics@users.noreply.github.com>
2025-02-24 11:52:19 -0800

    force pos needed to be more forced (limiting my potential fr)

--- include/arm.hpp
+++ include/arm.hpp
@@ -19,7 +19,7 @@ class Arm {
         static constexpr double AUTON = 200.0 * 100;
         static constexpr double ALLIANCE_SCORE = 191.0 * 100;
         static constexpr double PREALLIANCE_SCORE = 170.0 * 100;
-        static constexpr double FORCE_POS = 200.0 * 100;
+        static constexpr double FORCE_POS = 300.0 * 100;
         static constexpr double HOLD_POS = 90.0 * 100;
         static constexpr double MOGO_SCORE = 208.0 * 100;
         double last_pos = 0;
