6bdaa406666f360cab09508ac57b5d2980c17e45
CodeMyGame1 <pranesh@yosanai.com>
2025-02-26 19:10:20 -0800

    run test auton and (hopefully) fix odom

--- src/autons.cpp
+++ src/autons.cpp
@@ -46,11 +46,15 @@ void print_robot_pos(void* chassisVoid) {
 void test_auton() {
     chassis.setPose(0,0,0);
 
-    chassis.moveToPoint(0, 24, 1000, {.maxSpeed = 80});
-    waitd;
+    // chassis.moveToPoint(0, 24, 1000, {.maxSpeed = 80});
+    // waitd;
 
-    chassis.turnToHeading(90, 1000, {.maxSpeed = 80});
-    waitd;
+    // chassis.turnToHeading(45, 1000, {.maxSpeed = 80});
+    // waitd;
+
+    // chassis.moveToPoint(24, 48, 1000, {.maxSpeed = 80});
+
+    chassis.moveToPose(24, 24, 45, 1250, {.lead = 0.6, .maxSpeed = 80});
 }
 
 /**
