6184e973cf592a2fac43f0776341e21062949e8c
AaravDoesRobotics <164962937+AaravDoesRobotics@users.noreply.github.com>
2025-04-17 20:35:16 -0700

    wokred on little of red rush

--- include/arm.hpp
+++ include/arm.hpp
@@ -19,7 +19,7 @@ class Arm {
         static constexpr double SCORE_POS = 169.0 * 100; // 140 originally
         static constexpr double AUTON = 200.0 * 100;
         // the ring doesn't get stuck on arm poly
-        static constexpr double ALLIANCE_SCORE = 197.0 * 100;
+        static constexpr double ALLIANCE_SCORE = 220.69 * 100;
         static constexpr double INTAKE_LIFT = 238.0 * 100;
         static constexpr double PREALLIANCE_SCORE = 170.0 * 100;
         static constexpr double FORCE_POS = 300.0 * 100;
